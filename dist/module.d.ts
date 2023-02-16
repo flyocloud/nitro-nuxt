9e2f792bc7523239b061b6e86d86d2385d1362a8
Basil Suter <git@nadar.io>
2023-02-16 20:33:58 +0000

    build command

---
+++ dist/module.d.ts
@@ -0,0 +1,8 @@
+import * as _nuxt_schema from '@nuxt/schema';
+
+interface ModuleOptions {
+    token: string;
+}
+declare const _default: _nuxt_schema.NuxtModule<ModuleOptions>;
+
+export { ModuleOptions, _default as default };
---
+++ dist/types.d.ts
@@ -0,0 +1,10 @@
+
+import { ModuleOptions } from './module'
+
+declare module '@nuxt/schema' {
+  interface NuxtConfig { ['flyo']?: Partial<ModuleOptions> }
+  interface NuxtOptions { ['flyo']?: ModuleOptions }
+}
+
+
+export { ModuleOptions, default } from './module'
